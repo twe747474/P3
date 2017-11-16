65ec44c0109e630c49df357c5d5e45dab3868462
Chris Schaffer <soccer.schaffer@yahoo.com>
2017-11-16 14:49:36 -0700

    add dijkstra / forwarding table

--- router.h
+++ router.h
@@ -3,20 +3,37 @@
 //
 #include <sys/socket.h>
 #include <vector>
+#include <map>
 #ifndef P3_ROUTER_H
 #define P3_ROUTER_H
 
 void * createRouter(void *);
-struct neighbor{
-    int address;
-    int port;
+struct neighbor {
+    int address;    //src
+    int port;       //dest
     int cost;
+    neighbor(int s, int d, int w){
+        address = s;
+        port = d;
+        cost = w;
+    }
+    neighbor() = default;
+};
+struct lsp {    //link state packet
+    int src;
+    std::vector<neighbor> neighbors;
 };
 class router{
     std::string name;
     int udpPort;
+    int nRouters;
     std::vector<neighbor> myNeighbors;
     std::string home;
+    lsp lspacket;
+    struct lspList {
+        std::vector<struct lsp> lsps;
+    } lspackets;
+    std::map<int,int> fwdTable; //<dest,adjacentNode>   NOTE: if adjacentNode is this router then dest is connected to this router, send direct to dest
 
 public:
     void addNeighbor(neighbor n)
@@ -39,6 +56,14 @@ public:
     {
         return udpPort;
     }
+    void setNumRouters(int n)
+    {
+        nRouters = n;
+    }
+    int getNumRouters()
+    {
+        return nRouters;
+    }
     std::string getHome()
     {
         return home;
@@ -53,10 +78,35 @@ public:
     {
         return name;
     }
+    void setFwdTable(std::map<int,int> tmp)
+    {
+        fwdTable = tmp;
+    }
+    std::map<int,int> getFwdTable()
+    {
+        return fwdTable;
+    }
+    void addLSP(struct lsp tmp)
+    {
+        lspackets.lsps.push_back(tmp);
+    }
+    struct lspList getLSPlist()
+    {
+        return lspackets;
+    }
+    void setLSP(struct lsp tmp)
+    {
+        lspacket = tmp;
+    }
+    struct lsp getLSP()
+    {
+        return lspacket;
+    }
 
 };
 void digestMessage( std::string, router &);
 void createConnection(int , router &);
 void createUDP(int , router &);
 void WaitForNeighbors(int , router &);
+void createFwdTable(router &);
 #endif //P3_ROUTER_H
